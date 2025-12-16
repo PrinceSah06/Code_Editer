1e693f0bb128dc491a12c9ee62e26aa52fd1537b
PrinceSah06 <sahp4088@gmail.com>
2025-12-16 12:39:48 +0530

    Add client and server initial setup

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express()
+
+const PORT = process.env.PROT || 3000
+
+
+app.post('/',(req ,res)=>{
+    res.send('this is a basic route')
+})
+app.listen(PORT,()=>{
+    console.log('server is running on port 3000')
+})
